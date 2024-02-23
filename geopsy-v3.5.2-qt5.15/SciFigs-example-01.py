b8cd1109d8e93a9f89bfcbdab01f7b3abfe2990f
Imahn <imahn.shekhzadeh@hesge.ch>
2024-02-23 14:04:48 +0100

    isort & black

--- geopsy-v3.5.2-qt5.15/SciFigs-example-01.py
+++ geopsy-v3.5.2-qt5.15/SciFigs-example-01.py
@@ -1,29 +1,32 @@
 #!/opt/conda/bin/python3 -i
 
-import sys
 import logging
+import sys
 
+import GeopsyPyCoreWave as gp
 import GeopsyPySciFigs as sf
 import numpy as np
-import GeopsyPyCoreWave as gp
 
 logging.basicConfig(
-    filename='example.log', level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    filename="example.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
 )
-nmodes=1  # fundamental mode
+nmodes = 1  # fundamental mode
 
-freq=np.logspace(np.log10(1), np.log10(50), 500)
-omega=freq.copy()
-omega*=2*np.pi
+freq = np.logspace(np.log10(1), np.log10(50), 500)
+omega = freq.copy()
+omega *= 2 * np.pi
 
-h=np.array([10,20])
-vp=np.array([500,1000,3000])
-vs=np.array([200,600,1500])
-rho=np.array([2000,2000,2500])
+h = np.array([10, 20])
+vp = np.array([500, 1000, 3000])
+vs = np.array([200, 600, 1500])
+rho = np.array([2000, 2000, 2500])
 
 # slowRayleigh=gp.rayleighDispersionCurve(nmodes, 0, h, vp, vs, rho, omega)
-slowRayleighGroup=gp.rayleighDispersionCurve(nmodes, 1, h, vp, vs, rho, omega).squeeze()
+slowRayleighGroup = gp.rayleighDispersionCurve(
+    nmodes, 1, h, vp, vs, rho, omega
+).squeeze()
 logging.info(
     f"Rayleigh group velocities:\n{slowRayleighGroup}\nshape: "
     f"{slowRayleighGroup.shape}, type: {type(slowRayleighGroup)}"
