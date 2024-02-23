223dda8da8b6df0f85ae02697734e215ee19a4a5
Imahn <imahn.shekhzadeh@hesge.ch>
2024-02-23 14:07:07 +0100

    refactor file

--- geopsy-v3.5.2-qt5.15/SciFigs-example-01.py
+++ geopsy-v3.5.2-qt5.15/SciFigs-example-01.py
@@ -1,33 +1,40 @@
 #!/opt/conda/bin/python3 -i
 
 import logging
-import sys
 
 import GeopsyPyCoreWave as gp
-import GeopsyPySciFigs as sf
 import numpy as np
 
-logging.basicConfig(
-    filename="example.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
-nmodes = 1  # fundamental mode
 
-freq = np.logspace(np.log10(1), np.log10(50), 500)
-omega = freq.copy()
-omega *= 2 * np.pi
+def main() -> None:
+    """Main function."""
 
-h = np.array([10, 20])
-vp = np.array([500, 1000, 3000])
-vs = np.array([200, 600, 1500])
-rho = np.array([2000, 2000, 2500])
+    logging.basicConfig(
+        filename="example.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+    nmodes = 1  # fundamental mode
 
-# slowRayleigh=gp.rayleighDispersionCurve(nmodes, 0, h, vp, vs, rho, omega)
-slowRayleighGroup = gp.rayleighDispersionCurve(
-    nmodes, 1, h, vp, vs, rho, omega
-).squeeze()
-logging.info(
-    f"Rayleigh group velocities:\n{slowRayleighGroup}\nshape: "
-    f"{slowRayleighGroup.shape}, type: {type(slowRayleighGroup)}"
-)
+    freq = np.logspace(np.log10(1), np.log10(50), 500)
+    omega = freq.copy()
+    omega *= 2 * np.pi
+
+    h = np.array([10, 20])
+    vp = np.array([500, 1000, 3000])
+    vs = np.array([200, 600, 1500])
+    rho = np.array([2000, 2000, 2500])
+
+    # slowRayleigh=gp.rayleighDispersionCurve(nmodes, 0, h, vp, vs, rho, omega)
+    slowRayleighGroup = gp.rayleighDispersionCurve(
+        nmodes, 1, h, vp, vs, rho, omega
+    ).squeeze()
+
+    logging.info(
+        f"Rayleigh group velocities:\n{slowRayleighGroup}\nshape: "
+        f"{slowRayleighGroup.shape}, type: {type(slowRayleighGroup)}"
+    )
+
+
+if __name__ == "__main__":
+    main()
